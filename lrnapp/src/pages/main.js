1ebd53a1cb86f5c283bdbae98ace8817ad4560bd
zeeheb <zefutbrandao@gmail.comgit config --global user.email zefutbrandao@gmail.com>
2019-02-02 20:05:58 -0200

    header qse

--- lrnapp/src/pages/main.js
+++ lrnapp/src/pages/main.js
@@ -3,6 +3,11 @@ import React, { Component } from 'react';
 import { View, Text} from 'react-native';
 
 export default class Main extends Component {
+    static navigationOptions = {
+        title: "Marmitop",
+        
+    };
+    
     render() {
       return (
         <View>
